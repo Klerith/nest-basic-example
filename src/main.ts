f5fc8c5302b9922f1b5d570f967e3167ec14245a
Fernando Herrera <fernando.herrera85@gmail.com>
2022-11-06 21:45:38 -0500

    Port added

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,10 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+
+  await app.listen( PORT );
+
+  console.log(`Application running on port: ${ PORT }`);
 }
 bootstrap();
